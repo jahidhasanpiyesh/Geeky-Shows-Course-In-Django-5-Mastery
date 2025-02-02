e2099f30ffc0868fa38d84f33bd56331cf31c597
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2025-02-02 07:30:42 +0600

    Use Django Static Files CSS Javascript and Images
    Add to Static/css/template/html/js File and access code
    activate use to js and 2 way pic show this project

--- Mastery--02/core/settings.py
+++ Mastery--02/core/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR/'templates'
+STATIC_DIR = BASE_DIR/'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -37,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # I was a Creating App
-    'home',
+    'homeapp',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ Mastery--02/homeapp/static/homeapp/js/scripts.js
@@ -0,0 +1,3 @@
+function disp(){
+    alert("Hi My Name Is Javascripts")
+}
